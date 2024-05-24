c89bafcacdc8698ae1aac13adabcf618f20280ae
nagas <nagas@NAGASAI>
2024-05-24 22:13:35 +0530

    Add project files.

---
+++ EliteAcademyApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EliteAcademyApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EliteAcademyApplication/wwwroot/js/scripts.js
@@ -0,0 +1,71 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.bottom >= 0 &&
+            rect.left <= (window.innerWidth || document.documentElement.clientWidth) &&
+            rect.right >= 0
+        );
+    }
+
+    function addFadeInClass() {
+        var elements = document.querySelectorAll('.animated');
+        elements.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                console.log('Element is in viewport:', element);
+                element.classList.add('fadeIn');
+            }
+        });
+    }
+
+    // Add event listener for scrolling
+    window.addEventListener('scroll', function () {
+        console.log('Scroll event occurred');
+        addFadeInClass();
+    });
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
